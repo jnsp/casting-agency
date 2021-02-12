6b9f742e744fe87544b945ae6e3cfb40206ca9d8
Jangsea Park <jangsea.park@gmail.com>
2021-02-12 14:19:02 -0500

    Return json

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
-
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def check_health():
-    return 'healthy'
+    return jsonify({
+        'success': True,
+        'data': 'Healthy!',
+    }), 200
+
+
+@app.route('/movies')
+def get_movies():
+    movies = [1, 2, 3]
+    return jsonify({
+        'success': True,
+        'movies': movies,
+    }), 200

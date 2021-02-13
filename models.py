5a46ccd8352e9ad38fa7eebb1b127a30af29ce7c
Jangsea Park <jangsea.park@gmail.com>
2021-02-12 22:18:08 -0500

    Add Movie and Actor models

---
+++ models.py
@@ -0,0 +1,42 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), nullable=False)
+    release_date = db.Column(db.Date, nullable=False)
+
+    def __str__(self):
+        return f'<Movie(title={self.title}, release_date={self.release_date}>'
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+
+    def __init__(self, **kwargs):
+        super(Actor, self).__init__(**kwargs)
+        if self.age < 0:
+            raise ValidationError('Age is negative')
+
+    def __str__(self):
+        return f'<Actor(name={self.name}, age={self.age}, ' \
+                'gender={self.gender}>'
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class ValidationError(ValueError):
+    pass
---
+++ test_models.py
@@ -0,0 +1,65 @@
+from datetime import date
+import os
+import tempfile
+
+import pytest
+
+from app import app
+from models import db, Movie, Actor, ValidationError
+
+
+@pytest.fixture
+def init_test_db():
+    with tempfile.NamedTemporaryFile() as test_db:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + test_db.name
+        app.config['TESTING'] = True
+        db.init_app(app)
+        with app.app_context():
+            db.create_all()
+            yield
+            db.drop_all()
+
+    assert os.path.exists(test_db.name) is False
+
+
+def test_movie_model(init_test_db):
+    first_movie = Movie(title='TITLE1', release_date=date(2020, 1, 1))
+    first_movie.save()
+    second_movie = Movie(title='TITLE2', release_date=date(2020, 1, 2))
+    second_movie.save()
+
+    saved_movies = Movie.query.all()
+    assert len(saved_movies) == 2
+
+    first_saved_movie = Movie.query.get(1)
+    assert first_saved_movie.title == 'TITLE1'
+    assert first_saved_movie.release_date == date(2020, 1, 1)
+
+    second_saved_movie = Movie.query.get(2)
+    assert second_saved_movie.title == 'TITLE2'
+    assert second_saved_movie.release_date == date(2020, 1, 2)
+
+
+def test_actor_model(init_test_db):
+    first_actor = Actor(name='ACTOR1', age=10, gender='F')
+    first_actor.save()
+    second_actor = Actor(name='ACTOR2', age=20, gender='M')
+    second_actor.save()
+
+    saved_actors = Actor.query.all()
+    assert len(saved_actors) == 2
+
+    first_saved_actor = Actor.query.get(1)
+    assert first_saved_actor.name == 'ACTOR1'
+    assert first_saved_actor.age == 10
+    assert first_saved_actor.gender == 'F'
+
+    second_saved_actor = Actor.query.get(2)
+    assert second_saved_actor.name == 'ACTOR2'
+    assert second_saved_actor.age == 20
+    assert second_saved_actor.gender == 'M'
+
+
+def test_actor_non_negative_age(init_test_db):
+    with pytest.raises(ValidationError, match='Age is negative'):
+        Actor(age=-1)

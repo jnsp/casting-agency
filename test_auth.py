8f2cd4117e882e1a507db5957800c9a02663b80c
Jangsea Park <jangsea.park@gmail.com>
2021-02-15 18:56:15 -0500

    Reveal purpose of tests explicitly

--- test_auth.py
+++ test_auth.py
@@ -10,23 +10,31 @@ def app():
 
 
 def test_get_auth_token(app):
-    with app.test_request_context(headers={'Authorization': 'bearer TOKEN'}):
+    token_header = {'Authorization': 'bearer TOKEN'}
+
+    with app.test_request_context(headers=token_header):
         assert get_auth_token() == 'TOKEN'
 
 
 def test_autherror_without_auth_header(app):
-    with app.test_request_context():
+    no_header = {}
+
+    with app.test_request_context(headers=no_header):
         with pytest.raises(AuthError, match='No Authorization header'):
             get_auth_token()
 
 
 def test_autherror_when_auth_splited_improperly(app):
-    with app.test_request_context(headers={'Authorization': 'A B C'}):
+    too_many_splited_header = {'Authorization': 'A B C'}
+
+    with app.test_request_context(headers=too_many_splited_header):
         with pytest.raises(AuthError, match='Token splited too few or many'):
             get_auth_token()
 
 
 def test_autherror_when_auth_type_is_not_bearer(app):
-    with app.test_request_context(headers={'Authorization': 'basic TOKEN'}):
+    not_bearer_token_header = {'Authorization': 'basic TOKEN'}
+
+    with app.test_request_context(headers=not_bearer_token_header):
         with pytest.raises(AuthError, match='Not bearer auth type'):
             get_auth_token()

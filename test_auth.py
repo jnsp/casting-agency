eb80a4ea7ab1eedfe9dcfc6f457998378fe735f6
Jangsea Park <jangsea.park@gmail.com>
2021-02-16 02:06:12 -0500

    Get test jwt from auth0 server

--- test_auth.py
+++ test_auth.py
@@ -1,4 +1,7 @@
+from dotenv import load_dotenv
 import pytest
+import requests
+import os
 
 from app import create_app
 from auth import get_auth_token, AuthError
@@ -38,3 +41,22 @@ def test_autherror_when_auth_type_is_not_bearer(app):
     with app.test_request_context(headers=not_bearer_token_header):
         with pytest.raises(AuthError, match='Not bearer auth type'):
             get_auth_token()
+
+
+def get_test_jwt():
+    load_dotenv()
+    AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')
+    TEST_CLIENT_ID = os.getenv('TEST_CLIENT_ID')
+    TEST_CLIENT_SECRET = os.getenv('TEST_CLIENT_SECRET')
+    API_AUDIENCE = os.getenv('API_AUDIENCE')
+
+    url = f'https://{AUTH0_DOMAIN}/oauth/token'
+    headers = {'content-type': 'application/json'}
+    data = {
+        'client_id': TEST_CLIENT_ID,
+        'client_secret': TEST_CLIENT_SECRET,
+        'audience': API_AUDIENCE,
+        'grant_type': 'client_credentials',
+    }
+    res = requests.post(url, headers=headers, json=data)
+    return res.json()['access_token']
